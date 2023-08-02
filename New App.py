5a91e5042269dab94d6ef9c56049ebadcd0bcde5
otey247 <50851804+otey247@users.noreply.github.com>
2023-08-02 11:50:15 -0500

    Added predecessor and successor

--- New App.py
+++ New App.py
@@ -4,9 +4,9 @@ import json
 import base64
 from typing import Dict
 
-organization = ''
-project = ''
-pat = ''
+organization = 'jootey'
+project = 'Kroger%20Cloud%20Factory'
+pat = '4mjw246fzpp7lga7j2yld5myh5jam5trmrtuuomcti5cdg3dqwea'
 authorization = str(base64.b64encode(bytes(':'+pat, 'ascii')), 'ascii')
 
 headers = {
@@ -14,7 +14,7 @@ headers = {
     'Authorization': 'Basic '+authorization
 }
 
-def create_work_item(work_item_data, parent_id=None, new_epic_name=None) -> Dict[int, int]:
+def create_work_item(work_item_data, parent_id=None, new_epic_name=None, relationships=[], parent_id_mapping={}) -> Dict[int, int]:
     work_item_fields = work_item_data['fields']
     work_item_type = work_item_fields['System.WorkItemType']
     
@@ -60,11 +60,40 @@ def create_work_item(work_item_data, parent_id=None, new_epic_name=None) -> Dict
                 }
             }
         })
+    # Handle predecessor and successor relationships
+    for relation in relationships:
+        rel_type = relation.get('rel')
+        if rel_type == 'System.LinkTypes.Dependency-Reverse' or rel_type == 'System.LinkTypes.Dependency-Foward':
+            old_related_work_item_id = int(relation['url'].split('/')[-1])
+            if old_related_work_item_id in parent_id_mapping:
+                new_related_work_item_id = parent_id_mapping[old_related_work_item_id]
+                comment = relation.get('attributes', {}).get('comment', None)
+
+                # Now use this comment in your patch_data.append call
+                patch_data.append({
+                    'op': 'add',
+                    'path': '/relations/-',
+                    'value': {
+                        'rel': rel_type,
+                        'url': f'https://dev.azure.com/{organization}/{project}/_apis/wit/workItems/{new_related_work_item_id}',
+                        'attributes': {
+                            'comment': comment
+                        }
+                    }
+                })
+
 
     work_item_type = work_item_fields['System.WorkItemType']
     url = f'https://dev.azure.com/{organization}/{project}/_apis/wit/workitems/${work_item_type}?api-version=6.0'
     response = requests.patch(url, headers=headers, json=patch_data)
     response_data = response.json()
+    # Debugging lines
+    # print("Response Status:", response.status_code)
+    # print("Response Data:", response_data)
+
+    if "id" not in response_data:
+        print("Error: ID not found in the response.")
+        return
     new_work_item_id = response_data['id']
     old_work_item_id = work_item_data['id']
     return {old_work_item_id: new_work_item_id}
@@ -87,13 +116,16 @@ def create_work_items_from_folder(folder_path: str, new_epic_name: str) -> Dict[
         with open(file_path, 'r') as f:
             work_item_data = json.load(f)
 
+            # Extract relationships from the original work item
+            relationships = work_item_data.get('relations', [])
+
             parent_id = None
             if 'System.Parent' in work_item_data['fields']:
                 old_parent_id = work_item_data['fields']['System.Parent']
                 if old_parent_id in parent_id_mapping:
                     parent_id = parent_id_mapping[old_parent_id]
 
-            new_work_item = create_work_item(work_item_data, parent_id, new_epic_name)
+            new_work_item = create_work_item(work_item_data, parent_id, new_epic_name, relationships, parent_id_mapping)
             parent_id_mapping.update(new_work_item)
 
     print('Parent ID Mapping:', parent_id_mapping)
@@ -106,3 +138,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
